1db59f22083801ab12f99feb2c5b857514bdacad
Seba <117592896+KubikNuks@users.noreply.github.com>
2023-03-08 02:38:24 +0100

    Update saver.py
    Ive added some buttons to see how it all works

--- saver.py
+++ saver.py
@@ -1,23 +1,57 @@
 import os
 import pyautogui
+import numpy as np
+import tkinter as tk
 
-filename = 'screenshot.png'
+
+
+def take_screenshot():
+
+    filename = 'screenshot.png'
 
 # Check if the file already exists
-if os.path.exists(filename):
+    if os.path.exists(filename):
     # If it does, add a number to the end of the filename to make it unique
-    i = 1
-    while True:
-        new_filename = f'screenshot_{i}.png'
-        if not os.path.exists(new_filename):
-            break
-        i += 1
-    filename = new_filename
+        i = 1
+        while True:
+            new_filename = f'screenshot_{i}.png'
+            if not os.path.exists(new_filename):
+                break
+            i += 1
+        filename = new_filename
 
 # Capture the screen
-img = pyautogui.screenshot()
-
+    img = pyautogui.screenshot()
+    img_np = np.array(img)
 
 
 # Save the screenshot to a file
-img.save(filename)
+    img.save(filename)
+    print(f'Screenshot saved as {filename}')
+
+def nowy_prz():
+    button3 = tk.Button(root, text='Take Screenshot', command=on_click)
+    button3.pack()
+# Create a function to handle button clicks
+def on_click():
+    take_screenshot()
+
+def remove_button():
+    button.destroy()
+
+# Create the GUI window
+root = tk.Tk()
+root.title('Screenshot')
+
+# Create the screenshot button
+button = tk.Button(root, text='Take Screenshot', command=on_click)
+button.pack()
+
+add_button = tk.Button(root, text='Dodaj przycisk',command=nowy_prz)
+add_button.pack()
+
+button4 = tk.Button(root, text='Usun przycisk',command=remove_button)
+button4.pack()
+ 
+# Start the GUI loop
+root.mainloop()
